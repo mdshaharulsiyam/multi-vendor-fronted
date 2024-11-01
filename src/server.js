10befa8970bd60055231ed58dac308f5883c6edf
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-11-01 20:10:06 +0600

    fix issues

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 // export const server = "http://localhost:8000/api/v2";
 // export const ENDPOINT = "http://localhost:8000/";
 export const server = "https://multi-vendor-e-store-server.vercel.app/api/v2";
-export const ENDPOINT = "https://multi-vendor-e-store-server.vercel.app/";
+export const ENDPOINT = "https://multi-vendor-e-store-server.vercel.app";
 // export const backend_url = "https://multi-vendor-e-store-server.onrender.com/";
 export const backend_url = "https://drive.google.com/";
 
